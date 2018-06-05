1669c18081fe243203479733a08eb686172695d6
irman33 <irman.ahmetovic@kaplan.com>
2018-06-05 15:30:51 -0400

    basic Firebase auth implemented.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Redirect,
-  withRouter
-} from "react-router-dom";
-import base  from './base'
-import Header from './components/Header';
+import {  BrowserRouter as Router,
+          Route,
+          Link,
+          Redirect,
+          withRouter
+        } from "react-router-dom";
+import firebase from 'firebase';
+import base, { firebaseApp }  from './base'
+import Play from './components/Play';
+import Host from './components/Host';
+import Login from './components/Login';
 import './App.css';
 
 class App extends Component {
-  constructor() {
+  constructor() {Play
     super();
 
     this.state = {
@@ -19,17 +21,23 @@ class App extends Component {
     };
   }
 
-  // <div className="top" id="top">
-  //   <Header />
-  // </div>
-  // <div className="bottom" id="bottom" />
+  authHandler = async (authData) => {
+
+    console.log(authData);
+  }
+
+  authenticate = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebaseApp.auth().signInWithPopup(provider).then(this.authHandler);
+  }
 
   render() {
     return (
       <Router>
         <div className="game" id="gameArea">
-          <Route path="/login" component={Login} />
-          <Route path="/play" component={Play} />
+          <Route exact path="/" component={Play} />
+          <Route path="/login" render={ () => <Login authenticate={this.authenticate} /> } />
           <Route path="/host" component={Host} />
         </div>
       </Router>
